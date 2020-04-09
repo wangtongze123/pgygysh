4eaf6de7e1ba8fefbaec6e59cf6d89a2a4469cf8
wtzBlog <1649902712@qq.com>
2020-04-09 19:09:35 +0800

    1

---
+++ js/brand.js
@@ -0,0 +1,415 @@
+var headerList =[{
+		"title":"热门",
+		"t1":"魅惑美唇",
+		"t2":"塑造立体轮廓",
+		"t3":"热销气垫"
+	},
+	{
+		"title":"功效",
+		"t1":"魅惑美唇",
+		"t2":"塑造立体轮廓",
+		"t3":"热销气垫"
+	},
+	{
+		"title":"护肤",
+		"t1":"魅惑美唇",
+		"t2":"塑造立体轮廓",
+		"t3":"热销气垫"
+	},
+	{
+		"title":"彩妆",
+		"t1":"魅惑美唇",
+		"t2":"塑造立体轮廓",
+		"t3":"热销气垫"
+	},
+	{
+		"title":"香水",
+		"t1":"魅惑美唇",
+		"t2":"塑造立体轮廓",
+		"t3":"热销气垫"
+	},
+	{
+		"title":"工具",
+		"t1":"魅惑美唇",
+		"t2":"塑造立体轮廓",
+		"t3":"热销气垫"
+	},
+	{
+		"title":"男士护肤",
+		"t1":"魅惑美唇",
+		"t2":"塑造立体轮廓",
+		"t3":"热销气垫"
+	},
+	{
+		"title":"洗浴护体",
+		"t1":"魅惑美唇",
+		"t2":"塑造立体轮廓",
+		"t3":"热销气垫"
+	},
+	{
+		"title":"美发护发",
+		"t1":"魅惑美唇",
+		"t2":"塑造立体轮廓",
+		"t3":"热销气垫"
+	}]
+
+var brand = [{
+		"src": "img/index/brand_icon1.png",
+		"name": "蒲公英书海"
+	},
+	{
+		"src": "img/index/brand_icon1.png",
+		"name": "蒲公英书海"
+	},
+	{
+		"src": "img/index/brand_icon1.png",
+		"name": "蒲公英书海"
+	},
+	{
+		"src": "img/index/brand_icon1.png",
+		"name": "蒲公英书海"
+	},
+	{
+		"src": "img/index/brand_icon1.png",
+		"name": "蒲公英书海"
+	},
+	{
+		"src": "img/index/brand_icon1.png",
+		"name": "蒲公英书海"
+	},
+	{
+		"src": "img/index/brand_icon1.png",
+		"name": "蒲公英书海"
+	},
+	{
+		"src": "img/index/brand_icon1.png",
+		"name": "蒲公英书海"
+	},
+	{
+		"src": "img/index/brand_icon1.png",
+		"name": "蒲公英书海"
+	},
+	{
+		"src": "img/index/brand_icon1.png",
+		"name": "蒲公英书海"
+	},
+	{
+		"src": "img/index/brand_icon1.png",
+		"name": "蒲公英书海"
+	},
+	{
+		"src": "img/index/brand_icon1.png",
+		"name": "蒲公英书海"
+	},
+	{
+		"src": "img/index/brand_icon1.png",
+		"name": "蒲公英书海"
+	},
+	{
+		"src": "img/index/brand_icon1.png",
+		"name": "蒲公英书海"
+	},
+	{
+		"src": "img/index/brand_icon1.png",
+		"name": "蒲公英书海"
+	},
+	{
+		"src": "img/index/brand_icon1.png",
+		"name": "蒲公英书海"
+	}]
+
+var brand01 = [{
+		"src1": "img/index/brand01_img1.jpg",
+		"name1": "LANCOME",
+		"name2": "兰蔻新精华肌底液",
+		"price": "￥760",
+		"src2":"img/index/brand01_spic1.jpg"
+	},
+	{
+		"src1": "img/index/brand01_img1.jpg",
+		"name1": "LANCOME",
+		"name2": "兰蔻新精华肌底液",
+		"price": "￥760",
+		"src2":"img/index/brand01_spic1.jpg"
+	},
+	{
+		"src1": "img/index/brand01_img1.jpg",
+		"name1": "LANCOME",
+		"name2": "兰蔻新精华肌底液",
+		"price": "￥760",
+		"src2":"img/index/brand01_spic1.jpg"
+	}]
+
+
+var brand02 = [{
+		"src": "img/index/brand02_spic1.jpg",
+		"name1": "QHDXCBS",
+		"name2": "C语言从入门到精通",
+		"price": "￥69.00/150爱心"
+	}]
+
+var brand03 = [{
+		"src": "img/index/brand03_spic1.jpg",
+		"name1": "QHDXCBS",
+		"name2": "c++面向对象程序设计",
+		"price": "￥34.50/99爱心"
+	}]
+
+var brand04 = [{
+		"src": "img/index/brand04_spic1.jpg",
+		"name1": "JXGYCBS",
+		"name2": "大学物理（少学时）",
+		"price": "￥49.80/110爱心"
+	}]
+
+var brand05 = [{
+		"src": "img/index/brand05_spic1.jpg",
+		"name1": "GDJYCBS",
+		"name2": "高等数学-第七版",
+		"price": "￥39.30/99爱心"
+	}]
+
+var brand06 = [{
+		"src": "img/index/brand06_spic1.jpg",
+		"name1": "RMYDCBS",
+		"name2": "啊哈！算法",
+		"price": "￥45.00/120爱心"
+	}]
+
+var brand07 = [{
+		"src": "img/index/brand07_spic1.jpg",
+		"name1": "QHDXCBS",
+		"name2": "C语言课程设计与游戏开发",
+		"price": "￥39.00/99爱心"
+	}]
+
+var like = [{
+		"src": "img/index/like_img1.jpg",
+		"name1": "SHYYXYCBS",
+		"name2": "从零起步学吉他",
+		"price": "￥28.8/75爱心"
+}]
+
+var details = [{
+		"src": "img/detail/detail_product1.jpg",
+		"name1": "SEPHORACOLLECTION",
+		"name2": "丝芙兰快乐小萌猪玩趣小猪蹄十色眼影盘",
+		"price": "259"
+}]
+
+var car = [{
+		"src1": "img/shoppingCar/shoppingbag_lunbo1.jpg",
+		"name1": "LANCOME",
+		"name2": "html，css网页制作",
+		"price": "￥30/60爱心"
+	},
+	{
+		"src1": "img/shoppingCar/shoppingbag_lunbo2.jpg",
+		"name1": "DIOR",
+		"name2": "游戏开发与应用",
+		"price": "￥57.00/110爱心"
+	},
+	{
+		"src1": "img/shoppingCar/shoppingbag_lunbo3.jpg",
+		"name1": "LANCOME",
+		"name2": "html，css网页制作",
+		"price": "￥30/60爱心"
+	},
+	{
+		"src1": "img/shoppingCar/shoppingbag_lunbo4.jpg",
+		"name1": "LANCOME",
+		"name2": "html，css网页制作",
+		"price": "￥30/60爱心"
+	},
+	{
+		"src1": "img/shoppingCar/shoppingbag_lunbo5.jpg",
+		"name1": "LANCOME",
+		"name2": "html，css网页制作",
+		"price": "￥30/60爱心"
+	},
+	{
+		"src1": "img/shoppingCar/shoppingbag_lunbo5.jpg",
+		"name1": "LANCOME",
+		"name2": "html，css网页制作",
+		"price": "￥30/60爱心"
+	},
+	{
+		"src1": "img/shoppingCar/shoppingbag_lunbo4.jpg",
+		"name1": "LANCOME",
+		"name2": "html，css网页制作",
+		"price": "￥30/60爱心"
+	},
+	{
+		"src1": "img/shoppingCar/shoppingbag_lunbo3.jpg",
+		"name1": "LANCOME",
+		"name2": "html，css网页制作",
+		"price": "￥30/60爱心"
+	},
+	{
+		"src1": "img/shoppingCar/shoppingbag_lunbo2.jpg",
+		"name1": "DIOR",
+		"name2": "html，css网页制作",
+		"price": "￥30/60爱心"
+	},
+	{
+		"src1": "img/shoppingCar/shoppingbag_lunbo1.jpg",
+		"name1": "LANCOME",
+		"name2": "html，css网页制作",
+		"price": "￥30/60爱心"
+	},
+	{
+		"src1": "img/shoppingCar/shoppingbag_lunbo1.jpg",
+		"name1": "LANCOME",
+		"name2": "html，css网页制作",
+		"price": "￥30/60爱心"
+	},
+	{
+		"src1": "img/shoppingCar/shoppingbag_lunbo2.jpg",
+		"name1": "DIOR",
+		"name2": "html，css网页制作",
+		"price": "￥30/60爱心"
+	},
+	{
+		"src1": "img/shoppingCar/shoppingbag_lunbo3.jpg",
+		"name1": "LANCOME",
+		"name2": "html，css网页制作",
+		"price": "￥30/60爱心"
+	},
+	{
+		"src1": "img/shoppingCar/shoppingbag_lunbo4.jpg",
+		"name1": "LANCOME",
+		"name2": "html，css网页制作",
+		"price": "￥30/60爱心"
+	},
+	{
+		"src1": "img/shoppingCar/shoppingbag_lunbo5.jpg",
+		"name1": "LANCOME",
+		"name2": "html，css网页制作",
+		"price": "￥30/60爱心"
+	}]
+
+var listBrand = [{
+		"src": "img/list/list_img0.jpg",
+		"name": "将来的你，一定会感谢现在拼命的自己",
+		"price": "❤398.00"
+	},
+	{
+		"src": "img/list/list_img1.jpg",
+		"name": "城南旧事",
+		"price": "❤398.00"
+	},
+	{
+		"src": "img/list/list_img2.jpg",
+		"name": "昆虫记",
+		"price": "❤399.00"
+	},
+	{
+		"src": "img/list/list_img3.jpg",
+		"name": "餐厅",
+		"price": "❤398.00"
+	},
+	{
+		"src": "img/list/list_img4.jpg",
+		"name": "骆驼祥子",
+		"price": "❤239.00"
+	},
+	{
+		"src": "img/list/list_img5.jpg",
+		"name": "Moner",
+		"price": "❤69.00"
+	},
+	{
+		"src": "img/list/list_img4.jpg",
+		"name": "骆驼祥子",
+		"price": "❤239.00"
+	},
+	{
+		"src": "img/list/list_img1.jpg",
+		"name": "城南旧事",
+		"price": "❤398.00"
+	},
+	{
+		"src": "img/list/list_img2.jpg",
+		"name": "昆虫记",
+		"price": "❤399.00"
+	},
+	{
+		"src": "img/list/list_img3.jpg",
+		"name": "餐厅",
+		"price": "❤398.00"
+	},
+	{
+		"src": "img/list/list_img0.jpg",
+		"name": "将来的你，一定会感谢现在拼命的自己",
+		"price": "❤398.00"
+	},
+	{
+		"src": "img/list/list_img1.jpg",
+		"name": "城南旧事",
+		"price": "❤398.00"
+	},
+	{
+		"src": "img/list/list_img2.jpg",
+		"name": "昆虫记",
+		"price": "❤399.00"
+	},
+	{
+		"src": "img/list/list_img3.jpg",
+		"name": "餐厅",
+		"price": "❤398.00"
+	},
+	{
+		"src": "img/list/list_img4.jpg",
+		"name": "骆驼祥子",
+		"price": "❤239.00"
+	},
+	{
+		"src": "img/list/list_img5.jpg",
+		"name": "Moner",
+		"price": "❤69.00"
+	},
+	{
+		"src": "img/list/list_img4.jpg",
+		"name": "骆驼祥子",
+		"price": "❤239.00"
+	},
+	{
+		"src": "img/list/list_img1.jpg",
+		"name": "城南旧事",
+		"price": "❤398.00"
+	},
+	{
+		"src": "img/list/list_img2.jpg",
+		"name": "昆虫记",
+		"price": "❤399.00"
+	},
+	{
+		"src": "img/list/list_img3.jpg",
+		"name": "餐厅",
+		"price": "❤398.00"
+	},
+	{
+		"src": "img/list/list_img5.jpg",
+		"name": "Moner",
+		"price": "❤69.00"
+	},
+	{
+		"src": "img/list/list_img4.jpg",
+		"name": "骆驼祥子",
+		"price": "❤239.00"
+	},
+	{
+		"src": "img/list/list_img1.jpg",
+		"name": "城南旧事",
+		"price": "❤398.00"
+	},
+	{
+		"src": "img/list/list_img2.jpg",
+		"name": "昆虫记",
+		"price": "❤399.00"
+	},
+	{
+		"src": "img/list/list_img3.jpg",
+		"name": "餐厅",
+		"price": "❤398.00"
+	}]
